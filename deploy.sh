ac073908bf204ebe1bf1ec7b84d55d30ac69248d
Shiva Vamsi <shivavamsi.gudivada@gmail.com>
2019-01-28 14:29:52 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:shivavamsi/classic-snake-game.git master:gh-pages
+git push -f git@github.com:shivavamsi/smart-lending.git master:gh-pages
 
 cd -
